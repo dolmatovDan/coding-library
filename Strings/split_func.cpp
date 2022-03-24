e997e7f300b0c9714c6b32e72abcad93fde37e1d
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2022-03-24 09:17:15 +0300

    delete defines in split_func

--- Strings/split_func.cpp
+++ Strings/split_func.cpp
@@ -1,40 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fo3(i, s, n) for(int i = s; i < n; i++)
-#define fo2(i, a) for(auto i: a)
 #define len(a) (int)(a.size())
-#define vec vector
-#define pb push_back
-#define deb(x) cout << (#x) << ' ' << '=' << ' ' << (x) << endl
-typedef vector<int> vi;
 
-
-vec<string> split(string s, string sep=" ") {
-    vec<string> res;
+vector<string> split(string s, string sep = " ") {
+    vector<string> res;
     string term = "#";
     s = sep + term + s;
     int n = len(s);
-    vi z(n);
+    vector<int> z(n);
     z[0] = 0;
     int l = 0, r = 0;
-    fo3(i, 1, n) {
+    for(int i = 1; i < n; ++i) {
         if (i <= r) z[i] = min(z[i - l], r - i + 1);
-        while(i + z[i] < n and s[z[i]] == s[i + z[i]]) z[i]++;
+        while (i + z[i] < n and s[z[i]] == s[i + z[i]]) z[i]++;
         if (i + z[i] - 1 > r) {
             l = i;
             r = i + z[i] - 1;
         }
     }
     string cur = "";
-    fo3(i, len(term) + len(sep), len(s)) {
+    for(int i = len(term) + len(sep); i < len(s); ++i) {
         if ((z[i] == len(sep) and len(cur))) {
-            res.pb(cur);
+            res.push_back(cur);
             cur = "";
-        } else if(z[i] != len(sep)) {
+        } else if (z[i] != len(sep)) {
             cur += s[i];
         }
         if (z[i] != len(sep) and i == len(s) - 1) {
-            res.pb(cur);
+            res.push_back(cur);
         }
         if (z[i] == len(sep)) i += z[i] - 1;
     }
@@ -43,11 +36,12 @@ vec<string> split(string s, string sep=" ") {
 
 signed main()
 {
+
     string s, sep;
-    getline(cin, s);
-    cin >> sep;
-    vec<string> res;
-    if (sep != "#") res = split(s, sep);
-    else res = split(s);
-    fo2(i, res) cout << i << ' ';
+
+    s = "shklhdfa asdfkhj  ffdsfasdfh hfdjhfs";
+
+    vector<string> res;
+    res = split(s);
+    for(auto i: res) cout << i << ';';
 }
