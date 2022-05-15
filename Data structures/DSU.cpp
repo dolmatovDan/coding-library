56711a1ad7327048c07d855452b1b8d589882fcb
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2022-05-15 15:43:21 +0300

    upd dsu

--- Data structures/DSU.cpp
+++ Data structures/DSU.cpp
@@ -1,42 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fo(i, n) for(int i = 0; i < n; i ++)
-typedef vector<int> vi;
-vi p, r; // p - предки, r - ранги
 
-int get(int a) {
-    return p[a] = (a == p[a] ? a : get(p[a]));
-}
-
-void union_d(int _a, int _b) {
-    int a = get(_a);
-    int b = get(_b);
-    if (r[a] == r[b]) r[a]++;
-    if (r[a] > r[b]) {
-        p[b] = a;
-    } else {
-        p[a] = b;
+struct dsu {
+    int size;
+    vector<int> p, r;
+    void init(int n) {
+        size = n;
+        p.assign(n, 0);
+        r.assign(n, 0);
     }
-}
-    
+    int get(int a) {
+        if (a == p[a]) {
+            return a;
+        }
+        p[a] = get(p[a]);
+        return p[a];
+    }
 
-void solve() {
-    int n;
-    cin >> n;
-    p.resize(n);
-    r.resize(n, 0);
-    return;
-}
+    void union_s(int _a, int _b) {
+        int a = get(_a);
+        int b = get(_b);
+        if (r[a] == r[b]) r[a]++;
+        if (r[a] > r[b]) {
+            p[b] = a;
+        } else {
+            p[a] = b;
+        }
+    }
+};
 
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-        solve();
-    }
     return 0;
 }
