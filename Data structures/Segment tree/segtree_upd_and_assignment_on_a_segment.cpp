47d902b94d21aae86150ef85905fc263d323d863
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2022-05-19 11:25:41 +0300

    fix segtree

--- Data structures/Segment tree/segtree_upd_and_assignment_on_a_segment.cpp
+++ Data structures/Segment tree/segtree_upd_and_assignment_on_a_segment.cpp
@@ -1,45 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define all(a) a.begin(), a.end()
+#define len(a) ((int)(a.size()))
+#define F first
+#define S second
+
 typedef long long ll;
-const ll infll = 2e18;
+typedef long double ld;
 
 struct segtree {
     struct node {
         ll set, min;
     };
     vector<node> tree;
-    int size;
-    ll NO_OP = -infll;
-    ll NEUTRAL = 0;
-    void init(int n) {
+    int size, n;
+    ll NO_OP = LLONG_MIN;
+    ll NEUTRAL = LLONG_MAX;
+    void init(int _n) {
+        n = _n;
         size = 1;
         while(size < n) size *= 2;
-        tree.assign(2 * size, {0, 0});
+        tree.assign(size * 2, {0, 0});
     }
-    ll op_modify(ll a, ll b, int len) {
+    ll op_modify(ll a, ll b, ll len) {
         if (b == NO_OP) return a;
-        return b * len;
+        return b;
     }
     ll op_sum(ll a, ll b) {
-        return a + b;
+        return min(a, b);
     }
     void propagate(int x, int lx, int rx) {
-        if (tree[x].set == NO_OP or rx - lx == 1) return;
+        if (tree[x].set == NO_OP || rx - lx == 1) return;
         int m = (lx + rx) / 2;
-        tree[x * 2 + 1].set = op_modify(tree[x * 2 + 1].set, tree[x].set, 1);
-        tree[x * 2 + 1].min = op_modify(tree[2 * x + 1].min, tree[x * 2 + 1].set, m - lx);
-
-        tree[x * 2 + 2].set = op_modify(tree[x * 2 + 2].set, tree[x].set, 1);
-        tree[x * 2 + 2].min = op_modify(tree[2 * x + 2].min, tree[x * 2 + 1].set, rx - m);
+        tree[2 * x + 1].set = op_modify(tree[2 * x + 1].set, tree[x].set, 1);
+        tree[2 * x + 1].min = op_modify(tree[2 * x + 1].min, tree[x].set, m - lx);
+        tree[2 * x + 2].set = op_modify(tree[2 * x + 2].set, tree[x].set, 1);
+        tree[2 * x + 2].min = op_modify(tree[2 * x + 2].min, tree[x].set, rx - m);
         tree[x].set = NO_OP;
     }
-
     void mult(int l, int r, int v, int x, int lx, int rx) {
         propagate(x, lx, rx);
-        if (l >= rx or lx >= r) return;
-        if (lx >= l and rx <= r) {
+        if (l >= rx || lx >= r) return;
+        if (lx >= l && rx <= r) {
             tree[x].set = op_modify(tree[x].set, v, 1);
-            tree[x].min = op_modify(tree[x].min, tree[x].set, rx - lx);
+            tree[x].min = op_modify(tree[x].min, v, rx - lx);
             return;
         }
         int m = (lx + rx) / 2;
@@ -47,52 +51,56 @@ struct segtree {
         mult(l, r, v, x * 2 + 2, m, rx);
         tree[x].min = op_sum(tree[x * 2 + 1].min, tree[x * 2 + 2].min);
     }
-
     void mult(int l, int r, int v) {
         mult(l, r, v, 0, 0, size);
     }
-
     ll sum(int l, int r, int x, int lx, int rx) {
         propagate(x, lx, rx);
-        if (lx >= r || l >= rx) return NEUTRAL;
-        if (lx >= l && rx <= r) {
-            return tree[x].min;
-        }
+        if (l >= rx || lx >= r) return NEUTRAL;
+        if (lx >= l && rx <= r) return tree[x].min;
         int m = (lx + rx) / 2;
         ll m1 = sum(l, r, x * 2 + 1, lx, m);
         ll m2 = sum(l, r, x * 2 + 2, m, rx);
         return op_sum(m1, m2);
     }
-
     ll sum(int l, int r) {
         return sum(l, r, 0, 0, size);
     }
-    void print() {
-        for(auto x:tree) cout << x.min << ' ' << x.set << '\n';
-    }
 };
 
-signed main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+void solve() {
     int n, m;
     cin >> n >> m;
     segtree st;
     st.init(n);
-    int com;
     while (m--) {
+        int com;
         cin >> com;
         if (com == 1) {
             int l, r, v;
             cin >> l >> r >> v;
             st.mult(l, r, v);
-        } else {
+        }
+        else {
             int l, r;
             cin >> l >> r;
             cout << st.sum(l, r) << '\n';
         }
+    }
+    return;
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tt = 1;
+//    cin >> tt;
+    while(tt--) {
+        solve();
+#ifndef ONLINE_JUDGE
+        cout << "_______________________\n";
+#endif
     }
     return 0;
 }
