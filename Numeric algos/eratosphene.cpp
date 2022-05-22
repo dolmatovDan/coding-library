7ecd4b8774895e4e2554d35093af1f0bd53d4dcd
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2022-05-22 14:29:32 +0300

    upd sieve

--- Numeric algos/eratosphene.cpp
+++ Numeric algos/eratosphene.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> sieve(int n) {
+vector<bool> sieve1(int n) {
     vector<bool> sieve(n + 1, true);
     sieve[1] = false;
     for(int i = 2;i * i <= n;++i) {
@@ -11,14 +11,29 @@ vector<int> sieve(int n) {
             }
         }
     }
+    return sieve;
+}
+
+vector<int> sieve2(int n) {
+    vector<bool> sieve(n + 1, true);
+    sieve[1] = false;
     vector<int> res;
-    for(int i = 1;i <= n;++i) {
-        if (sieve[i]) res.push_back(i);
+    for(int i = 2;i <= n;++i) {
+        if (sieve[i]) {
+            res.push_back(i);
+            for(int j = i * i;j <= n;j+=i) {
+                sieve[j] = false;
+            }
+        }
     }
     return res;
-}
+} 
 
 int main()
 {
+    int n;
+    cin >> n;
+    vector<int> a = sieve2(n);
+    for(int &x:a) cout << x << '\n';
     return 0;
 }
